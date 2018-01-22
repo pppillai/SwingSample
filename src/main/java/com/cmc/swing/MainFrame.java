9dcebf4c55e4ee4aced68dd24ff6582819ed2eea
Pradeep Pillai <PPillai@birst.com>
2018-01-22 07:42:36 +0530

    adding button

--- src/main/java/com/cmc/swing/MainFrame.java
+++ src/main/java/com/cmc/swing/MainFrame.java
@@ -1,12 +1,16 @@
 package com.cmc.swing;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class MainFrame extends JFrame {
 
   public MainFrame() {
     super("MyTitle");
     setSize(300, 300);
+    Container content = getContentPane();
+    content.add(new JButton("Button One"));
+
   }
 
 }
