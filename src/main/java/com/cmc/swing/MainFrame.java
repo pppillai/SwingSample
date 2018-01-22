c4571fca62d2aacab4041a7515a22f6b48abc2a2
Pradeep Pillai <PPillai@birst.com>
2018-01-22 07:53:14 +0530

    adding text area

--- src/main/java/com/cmc/swing/MainFrame.java
+++ src/main/java/com/cmc/swing/MainFrame.java
@@ -8,9 +8,17 @@ public class MainFrame extends JFrame {
   public MainFrame() {
     super("MyTitle");
     setSize(300, 300);
-    Container content = getContentPane();
-    content.add(new JButton("Button One"));
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
+    Container content = getContentPane();
+    content.setLayout(new BorderLayout());
+
+    JPanel jpanel = new JPanel(new FlowLayout());
+    jpanel.add(new JButton("ButtonOne"));
+    jpanel.add(new JButton("ButtonTwo"));
+    jpanel.add(new JButton("ButtonThree"));
+    content.add(jpanel, BorderLayout.SOUTH);
+    content.add(new JScrollPane(new JTextArea()), BorderLayout.CENTER);
   }
 
 }
